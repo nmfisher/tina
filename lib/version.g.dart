0f1cd4855e921933bfac11eff18ec7f17adba5e8
Nick Fisher <nick.fisher@avinium.com>
2026-08-31 22:06:24 +0800

    chore(release): bump version to 0.5.0

--- lib/version.g.dart
+++ lib/version.g.dart
@@ -2,4 +2,4 @@
 // with `dart run tool/generate_version.dart` after bumping pubspec.yaml.
 
 /// tina's version, from pubspec.yaml at build time.
-const String tinaVersion = '0.4.1';
+const String tinaVersion = '0.5.0';
